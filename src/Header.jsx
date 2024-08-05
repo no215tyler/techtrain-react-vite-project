c5c9bee4a9a64ac0b3a9e7149edd738a5e02ef07
no215tyler <no.215.tyler@gmail.com>
2024-08-06 00:12:37 +0900

    ✨Headerコンポーネント作成

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <h2>掲示板</h2>
+        <a href="">スレッドを立てる</a>
+      </div>
+    </>
+  );
+};
+
+export default Header;

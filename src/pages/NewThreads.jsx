d37c5811469de623735643417ce271bc19110579
no215tyler <no.215.tyler@gmail.com>
2024-08-24 17:46:23 +0900

    ✨ スレッド新規作成ロジックの実装

--- src/pages/NewThreads.jsx
+++ src/pages/NewThreads.jsx
@@ -1,13 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "/src/index.css";
 import classes from "../styles/NewThreads.module.css";
 const NewThreads = () => {
+  const [title, setTitle] = useState("");
+  const navigate = useNavigate();
+  const createThreads = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch(
+      "https://railway.bulletinboard.techtrain.dev/threads",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title }),
+      }
+    );
+    if (response.ok) {
+      console.log("スレッド作成完了");
+      navigate("/");
+    }
+  };
+
   return (
     <div className={classes.threadsWrapper}>
       <h1 className={classes.threads_title}>スレッド新規作成</h1>
       <div className={classes.formContainer}>
-        <form>
-          <input type="text" required className={classes.input} />
+        <form onSubmit={createThreads}>
+          <input
+            type="text"
+            required
+            className={classes.input}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="スレッドタイトルを入力してください"
+          />
           <div className={classes.buttonContainer}>
             <Link to="/" className={classes.backToButton}>
               Topへ戻る

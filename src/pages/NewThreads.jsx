1a605ec8894b12485689c8c5b9059fb62ce9ec68
no215tyler <no.215.tyler@gmail.com>
2024-08-24 17:30:31 +0900

    ✨ スレッド新規投稿画面フロント実装

--- src/pages/NewThreads.jsx
+++ src/pages/NewThreads.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
 import "/src/index.css";
+import classes from "../styles/NewThreads.module.css";
 const NewThreads = () => {
   return (
-    <>
-      <h1 className="threads_title">new threads</h1>
-    </>
+    <div className={classes.threadsWrapper}>
+      <h1 className={classes.threads_title}>スレッド新規作成</h1>
+      <div className={classes.formContainer}>
+        <form>
+          <input type="text" required className={classes.input} />
+          <div className={classes.buttonContainer}>
+            <Link to="/" className={classes.backToButton}>
+              Topへ戻る
+            </Link>
+            <button className={classes.submit}>作成</button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 

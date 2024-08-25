a36a5b80e5df30885ed559f774d2ccf51d85979b
no215tyler <no.215.tyler@gmail.com>
2024-08-25 20:57:51 +0900

    ✨ スレッド内コメント投稿機能の実装

--- src/pages/Threads.jsx
+++ src/pages/Threads.jsx
@@ -1,19 +1,45 @@
 import { useEffect, useState } from "react";
 import classes from "../styles/Threads.module.css";
 import { useLocation } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Threads = () => {
   const location = useLocation();
-  console.log(`スレタイ: ${location.state.thread.title}`);
-  console.log(location);
   const [comment, setComment] = useState({ posts: ["Loading..."] });
+  const [addComment, setAddComment] = useState("");
   const fetchTread = async () => {
     const response = await fetch(
       `https://railway.bulletinboard.techtrain.dev/threads/${location.state.thread.id}/posts`
     );
     const threadData = await response.json();
     setComment(threadData);
-    console.log(threadData);
+  };
+
+  const postComment = async () => {
+    if (addComment == "") {
+      alert("送信するには文字の入力が必要です。");
+      return;
+    }
+    try {
+      const response = await fetch(
+        `https://railway.bulletinboard.techtrain.dev/threads/${location.state.thread.id}/posts`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ post: addComment }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("レスポンスエラー");
+      }
+
+      setAddComment("");
+      fetchTread();
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   useEffect(() => {
@@ -21,11 +47,37 @@ const Threads = () => {
   }, []);
   return (
     <>
-      <div className={classes.header_title}>Threads</div>
-      <div className={classes.threadWrapper}>{location.state.thread.title}</div>
-      {comment.posts.map((post, index) => {
-        return <div key={index}>{post.post}</div>;
-      })}
+      <Helmet>
+        <title>{`Railway React掲示板｜${location.state.thread.title}`}</title>
+      </Helmet>
+      <div className={classes.threadWrapper}>
+        <div className={classes.commentWrapper}>
+          <div className={classes.threadTitle}>
+            {location.state.thread.title}
+          </div>
+          {comment.posts.map((post, index) => {
+            return (
+              <div key={index} className={classes.comment}>
+                {`> ${post.post}`}
+              </div>
+            );
+          })}
+        </div>
+        <div className={classes.postComment}>
+          <textarea
+            placeholder="投稿しよう！"
+            value={addComment}
+            id="textarea"
+            onChange={(e) => setAddComment(e.target.value)}
+          />
+          <button
+            className={classes.submitButton}
+            onClick={() => postComment()}
+          >
+            投稿
+          </button>
+        </div>
+      </div>
     </>
   );
 };
